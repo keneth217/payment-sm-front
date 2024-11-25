cd2baa7d149b1c1e8cba41eabdb2eda0c35528cf
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-25 13:17:54 +0300

    handle response

--- src/app/payment-success/payment-success.component.ts
+++ src/app/payment-success/payment-success.component.ts
@@ -9,23 +9,5 @@ import {ActivatedRoute, Router} from "@angular/router";
   styleUrl: './payment-success.component.css'
 })
 export class PaymentSuccessComponent {
-  constructor(private route: ActivatedRoute, private router: Router) {}
 
-  ngOnInit() {
-    // Capture the payment reference and status from the URL query params
-    this.route.queryParams.subscribe((params) => {
-      const reference = params['reference'];
-      const status = params['status']; // 'success' or 'failure'
-
-      if (status === 'success') {
-        // Handle successful payment logic here
-        console.log('Payment Successful', reference);
-        // Optionally redirect to a confirmation page or dashboard
-        this.router.navigate(['/success']);
-      } else {
-        console.log('Payment Failed');
-        // Optionally show an error message or retry payment
-      }
-    });
-  }
 }
