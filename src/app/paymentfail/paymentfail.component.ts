6391b6923bf5783001ddad37d01ff618ce692a3e
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-25 11:32:42 +0300

    add routes and components

---
+++ src/app/paymentfail/paymentfail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paymentfail',
+  standalone: true,
+  imports: [],
+  templateUrl: './paymentfail.component.html',
+  styleUrl: './paymentfail.component.css'
+})
+export class PaymentfailComponent {
+
+}
