adfb01353d1feed7e55f848abc1a1b45dd467f45
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-25 11:32:12 +0300

    add routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,47 +13,5 @@ import { AngularToastifyModule, ToastService } from 'angular-toastify';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  isPaying: boolean = false;
-  payForm!: FormGroup;
 
-  constructor(
-    private fb: FormBuilder,
-    private router: Router,
-    private service: PaymentService,
-    private toast: ToastService
-  ) {
-    this.payForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      amount: ['', [Validators.required, Validators.pattern('^[0-9]+(\\.[0-9]{1,2})?$')]]
-    });
-  }
-
-  payNow() {
-    if (this.payForm.valid) {
-      const paymentData = this.payForm.value;
-      console.log(paymentData)
-      this.isPaying = true;
-
-      this.service.payNow(this.payForm.value.email,this.payForm.value.amount).subscribe(
-        (response: PaymentResponse) => {
-          if (response.status) {
-            this.toast.success(response.message + ": You are being redirected to payment page");
-
-            window.location.href = response.data.authorization_url; // Redirect to Paystack payment page//after payment is success i want to return to another page
-          } else {
-            this.toast.error('Payment initialization failed!');
-          }
-        },
-        (error) => {
-          console.error('Error initializing payment:', error);
-          this.toast.error(error.error?.errorMessage || 'Something went wrong!');
-        },
-        () => {
-          this.isPaying = false;
-        }
-      );
-    } else {
-      this.toast.warn('Please fill in all required fields!');
-    }
-  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import {PaymentSuccessComponent} from "./payment-success/payment-success.component";
 import {CallbackComponent} from "./callback/callback.component";
+import {PaymentFormComponent} from "./payment-form/payment-form.component";
+import {PaymentfailComponent} from "./paymentfail/paymentfail.component";
 
 export const routes: Routes = [
+
+  { path: "", redirectTo: "pay", pathMatch: "full" },
+  { path: 'pay', title: 'Login Page', component: PaymentFormComponent },
   {path:'success',component:PaymentSuccessComponent},
+  {path:'fail',component:PaymentfailComponent},
   {path:'payment/callback',component:CallbackComponent}
 ];
