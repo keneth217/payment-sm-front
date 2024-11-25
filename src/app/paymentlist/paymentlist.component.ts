a928e8aeead8ae71c136b37a79fed2a00553e895
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-25 11:32:34 +0300

    add routes and components

---
+++ src/app/paymentlist/paymentlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paymentlist',
+  standalone: true,
+  imports: [],
+  templateUrl: './paymentlist.component.html',
+  styleUrl: './paymentlist.component.css'
+})
+export class PaymentlistComponent {
+
+}
